725f00ee2f2befeedfb1d79e23c540231a8164cb
aravindveluri <aravindv178@gmail.com>
2019-12-14 00:34:13 +0530

    Check Input

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,21 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
-
+void UBullCowCartridge::InitGame(){
+    HiddenWord = TEXT("punks");
+    Lives = 4;
+}
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to the bull cows game!"));
-    PrintLine(TEXT("Enter your input and press ENTER:"));
-    HiddenWord = TEXT("punks");
+    PrintLine(TEXT("Guess the 5 letter word you punk"));
+    InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    if(HiddenWord == Input){
-        PrintLine("That's right u punks!");
+    if(Input.Len() != HiddenWord.Len()){
+        PrintLine(FString::Printf(TEXT("Guess should be of length %i") , (HiddenWord.Len())));
+    }
+    else if(HiddenWord == Input){
+        PrintLine(TEXT("That's right u punks!"));
     }
     else{
-        PrintLine("Nah, wrong answer");
+        PrintLine(TEXT("Nah, wrong answer"));
     }
 }
