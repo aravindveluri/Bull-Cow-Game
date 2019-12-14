26890c45901f25e59c845de1e6e46e448e674732
aravindveluri <aravindv178@gmail.com>
2019-12-14 18:36:04 +0530

    Add lives

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,27 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 void UBullCowCartridge::InitGame(){
-    HiddenWord = TEXT("punks");
-    Lives = 4;
+    HiddenWord = TEXT("lol");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to the bull cows game!"));
+    PrintLine(TEXT("Guess the %i letter word. You have %i lives"), HiddenWord.Len(), Lives);
 }
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to the bull cows game!"));
-    PrintLine(TEXT("Guess the 5 letter word you punk"));
     InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    if(Input.Len() != HiddenWord.Len()){
-        PrintLine(FString::Printf(TEXT("Guess should be of length %i") , (HiddenWord.Len())));
-    }
-    else if(HiddenWord == Input){
-        PrintLine(TEXT("That's right u punks!"));
+    if(bGameOver){
+        ClearScreen();
+        InitGame();
     }
     else{
-        PrintLine(TEXT("Nah, wrong answer"));
-    }
+        if(Input.Len() != HiddenWord.Len()){
+            PrintLine(FString::Printf(TEXT("Guess should be of length %i") , (HiddenWord.Len())));
+        }
+        else if(HiddenWord == Input){
+            PrintLine(TEXT("That's right u punks!"));
+            EndGame();
+        }
+        else{
+            if(Lives > 0){
+                PrintLine(TEXT("Wrong asnwer, you have %i lives left."), --Lives);
+            }
+            else{
+                PrintLine(TEXT("You Lost!"));
+                EndGame();
+            }
+        }
+    }    
+}
+void UBullCowCartridge::EndGame(){
+    //We're in the endgame now lol
+    bGameOver = true;
+    PrintLine(TEXT("Game Over! Press Enter to continue..."));
 }
